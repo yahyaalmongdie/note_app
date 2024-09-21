4cc6d9c42392e464b815ce3672ddc70171bfe33b
yahya almongdie <yahyaalmongdie@gmail.com>
2024-09-21 19:43:42 +0300

    custom app bar

--- lib/features/home/presentation/view/widget/home_view_body.dart
+++ lib/features/home/presentation/view/widget/home_view_body.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/features/home/presentation/view/widget/custom_app_bar.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          CustomAppBar()
+        ],
+      ),
+    );
   }
 }
