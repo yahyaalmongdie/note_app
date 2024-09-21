b3a3462566839352ca19e741a5c116d76d7019fd
yahya almongdie <yahyaalmongdie@gmail.com>
2024-09-21 20:16:16 +0300

    create custom note icon

---
+++ lib/features/home/presentation/view/widget/note_item.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+      decoration: BoxDecoration(
+          color: const Color(0xffffcc80),
+          borderRadius: BorderRadius.circular(16)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+              contentPadding: const EdgeInsets.all(0),
+              title: const Text(
+                "Flutter tips",
+                style: TextStyle(fontSize: 26, color: Colors.black),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: Text(
+                  "Build your career with  Yahya Almongdie",
+                  style: TextStyle(
+                      fontSize: 18, color: Colors.black.withOpacity(.5)),
+                ),
+              ),
+              trailing: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    FontAwesomeIcons.trash,
+                    color: Colors.black,
+                    size: 24,
+                  ))),
+   
+          Text("May 21,2022",
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.black.withOpacity(.5),
+              ))
+        ],
+      ),
+    );
+  }
+}
