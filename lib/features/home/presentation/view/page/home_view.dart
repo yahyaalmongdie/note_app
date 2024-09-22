30b1d42fc057d33e02ea4511d534d30423192e35
yahya almongdie <yahyaalmongdie@gmail.com>
2024-09-22 20:07:21 +0300

    show add note  model bottom sheet

--- lib/features/home/presentation/view/page/home_view.dart
+++ lib/features/home/presentation/view/page/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/features/home/presentation/view/widget/add_note_bottom_sheet.dart';
 import 'package:note_app/features/home/presentation/view/widget/home_view_body.dart';
 
 class HomeView extends StatelessWidget {
@@ -11,7 +12,13 @@ class HomeView extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
           shape:
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
-          onPressed: () {},
+          onPressed: () {
+            showModalBottomSheet(
+                context: context,
+                builder: (context) {
+                  return const AddNoteBottomSheet();
+                });
+          },
           child: const Icon(Icons.add)),
     );
   }
