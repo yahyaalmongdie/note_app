772733acf0ea3a7b6bee977ce569c8bf8fb3dde5
yahya almongdie <yahyaalmongdie@gmail.com>
2024-09-22 20:12:40 +0300

    add border radius to bottom sheet

--- lib/features/home/presentation/view/page/home_view.dart
+++ lib/features/home/presentation/view/page/home_view.dart
@@ -14,6 +14,10 @@ class HomeView extends StatelessWidget {
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
           onPressed: () {
             showModalBottomSheet(
+                shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(16),
+                        topRight: Radius.circular(16))),
                 context: context,
                 builder: (context) {
                   return const AddNoteBottomSheet();
