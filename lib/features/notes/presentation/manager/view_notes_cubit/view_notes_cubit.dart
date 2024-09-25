625c7572267c2bbb99d96fafa147d2aae260cfe8
yahya almongdie <yahyaalmongdie@gmail.com>
2024-09-25 18:01:22 +0300

    create view note cubit

--- lib/features/notes/presentation/manager/view_notes_cubit/view_notes_cubit.dart
+++ lib/features/notes/presentation/manager/view_notes_cubit/view_notes_cubit.dart
@@ -1,5 +1,7 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:note_app/core/utils/constant.dart';
 import 'package:note_app/features/notes/data/model/note_model.dart';
 
 part 'view_notes_state.dart';
@@ -7,4 +9,15 @@ part 'view_notes_state.dart';
 class ViewNotesCubit extends Cubit<ViewNotesState> {
   ViewNotesCubit() : super(ViewNotesInitial());
 
+  fetchNotes() {
+    emit(ViewNoteLoading());
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+
+      List<NoteModel> notes = notesBox.values.toList();
+      emit(ViewNoteSuccess(notes: notes));
+    } catch (e) {
+      emit(ViewNoteFailure(errorMessage: e.toString()));
+    }
+  }
 }
